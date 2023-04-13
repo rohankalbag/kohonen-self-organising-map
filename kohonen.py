6df296de854327493bc6fec26cf3a6a3a3e5d582
rohankalbag <kalbag.rohan.r@gmail.com>
2023-04-14 00:35:19 +0530

    added working som code

--- kohonen.py
+++ kohonen.py
@@ -1,4 +1,8 @@
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 
 class kohonen:
     def __init__(self, length, breadth, learning_rate, max_iter):
@@ -7,17 +11,21 @@ class kohonen:
         self.learning_rate = learning_rate
         self.max_iter = max_iter
         self.curr_iter = 0
+    
+    def norm(self, x, y):
+        return np.sum((x - y)**2)
 
     def find_best_matching_unit(self, x):
-        running_min = np.linalg.norm(self.kohonen_map[0,0], x)
+        running_min = self.norm(self.kohonen_map[0, 0], x)
         bmu = (0, 0)
         for i in range(self.length):
             for j in range(self.breadth):
-                if(np.linalg.norm(self.kohonen_map[i,j], x) < running_min):
+                if(self.norm(self.kohonen_map[i,j], x) < running_min):
                     bmu = (i, j)
-                    running_min= np.linalg.norm(self.kohonen_map[i,j], x)
+                    running_min = self.norm(self.kohonen_map[i,j], x)
 
         return bmu
+        
     
     def curr_lr(self):
         clr = self.learning_rate*(1 - (self.curr_iter/self.max_iter))
@@ -27,12 +35,12 @@ class kohonen:
         sigma_t = (self.length**2 + self.breadth**2)*np.exp(-self.curr_iter/self.max_iter)/16
         return np.exp(-(del_x**2 + del_y**2)/2/sigma_t)
 
-    def update_weights(self, bmu):
-        curr_lr = self.curr_lr(self.curr_iter)
+    def update_weights(self, bmu, x):
+        curr_lr = self.curr_lr()
         for i in range(self.length):
             for j in range(self.breadth):
-                delta_w_ij = curr_lr*(self.kohonen_map[bmu[0], bmu[1]] - self.kohonen_map[i, j]) 
-                delta_w_ij = self.neighbourhood_func(self.curr_iter, abs(bmu[0] - i), abs(bmu[1] - j))*delta_w_ij
+                delta_w_ij = curr_lr*(x - self.kohonen_map[i, j]) 
+                delta_w_ij = self.neighbourhood_func(abs(bmu[0] - i), abs(bmu[1] - j))*delta_w_ij
                 self.kohonen_map[i, j] += delta_w_ij
                 
 
@@ -40,11 +48,11 @@ class kohonen:
         self.kohonen_map = np.random.rand(self.length, self.breadth, img.shape[2])
         for i in range(self.max_iter):
             self.curr_iter = i
-            for u in img.shape[0]:
-                for v in img.shape[1]:
+            for u in tqdm(range(img.shape[0])):
+                for v in range(img.shape[1]):
                     x = img[u, v]
                     bmu = self.find_best_matching_unit(x)
-                    self.update_weights(bmu)
+                    self.update_weights(bmu, x)
             
     def generate_coded_image(self):
         return self.kohonen_map
