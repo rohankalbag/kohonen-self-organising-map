881f6a9ffee61fb7477b1e1fb45e43ebc433b828
rohankalbag <kalbag.rohan.r@gmail.com>
2023-04-20 01:47:04 +0530

    added linux executable

--- generate_executable.sh
+++ generate_executable.sh
@@ -1,4 +1,12 @@
+if [ -d "gnr602" ]; then
+    echo "virtual environment exists"
+else
+    python3 -m venv gnr602 
+fi
+
 rm *.spec
 rm -rf ./build
 rm -rf ./dist
-pyinstaller -F --paths=~/Desktop/kohonen-self-organising-map/gnr602/lib/python3.8/site-packages kohonen.py --hidden-import='PIL._tkinter_finder'
+source gnr602/bin/activate
+pip install -r requirements.txt
+pyinstaller -F --paths=./gnr602/lib/python3.8/site-packages kohonen.py --hidden-import='PIL._tkinter_finder'
