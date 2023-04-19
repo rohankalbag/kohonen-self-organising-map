1acca66374cf08eb9b4bbac523960e425742069e
rohankalbag <kalbag.rohan.r@gmail.com>
2023-04-20 00:26:34 +0530

    added user interface

--- kohonen.py
+++ kohonen.py
@@ -3,7 +3,14 @@ import numba
 from numba_progress import ProgressBar
 import sys
 import os
-
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from tkinter import messagebox
+import scipy
+import time
 
 def blockPrint():
     sys.stdout = open(os.devnull, 'w')
@@ -38,24 +45,24 @@ def curr_lr(learning_rate, curr_iter, max_iter):
 
 
 @numba.njit(nogil=True)
-def neighbourhood_func(del_x, del_y, length, breadth, curr_iter, max_iter):
-    sigma_t = (length**2 + breadth**2)*np.exp(-curr_iter/max_iter)/16
+def neighbourhood_func(del_x, del_y, length, breadth, curr_iter, max_iter, neighbourhood_spread):
+    sigma_t = (length**2 + breadth**2)*np.exp(-curr_iter/max_iter)*neighbourhood_spread
     return np.exp(-(del_x**2 + del_y**2)/2/sigma_t)
 
 
 @numba.njit(nogil=True)
-def update_weights(bmu, x, length, breadth, kohonen_map, curr_iter, max_iter, learning_rate):
+def update_weights(bmu, x, length, breadth, kohonen_map, curr_iter, max_iter, learning_rate, neighbourhood_spread):
     clr = curr_lr(learning_rate, curr_iter, max_iter)
     for i in range(length):
         for j in range(breadth):
             delta_w_ij = clr*(x - kohonen_map[i, j])
             delta_w_ij = neighbourhood_func(
-                abs(bmu[0] - i), abs(bmu[1] - j), length, breadth, curr_iter, max_iter)*delta_w_ij
+                abs(bmu[0] - i), abs(bmu[1] - j), length, breadth, curr_iter, max_iter, neighbourhood_spread)*delta_w_ij
             kohonen_map[i, j] += delta_w_ij
 
 
 @numba.njit(nogil=True)
-def fit(length, breadth, img, max_iter, learning_rate, progress):
+def fit(length, breadth, img, max_iter, learning_rate, neighbourhood_spread, progress):
     kohonen_map = np.random.rand(length, breadth, img.shape[2])*255
     for i in range(max_iter):
         curr_iter = i
@@ -64,7 +71,7 @@ def fit(length, breadth, img, max_iter, learning_rate, progress):
                 x = img[u, v]
                 bmu = find_best_matching_unit(x, length, breadth, kohonen_map)
                 update_weights(bmu, x, length, breadth, kohonen_map,
-                               curr_iter, max_iter, learning_rate)
+                               curr_iter, max_iter, learning_rate, neighbourhood_spread)
         progress.update(1)
     return kohonen_map
 
@@ -86,71 +93,146 @@ def generate_image_from_coded(coded_img, kohonen_map):
         shape=(coded_img.shape[0], coded_img.shape[1], kohonen_map.shape[2]))
     for i in range(coded_img.shape[0]):
         for j in range(coded_img.shape[1]):
-            m = np.array(coded_image[i, j], dtype=np.int32)
+            m = np.array(coded_img[i, j], dtype=np.int32)
             reconstructed[i, j] = kohonen_map[m[0], m[1]]
     return reconstructed
 
 
-def generate_kohonen(img, length, breadth, lr, maxiter):
+def generate_kohonen(img, length, breadth, lr, maxiter, spread):
 
     blockPrint()
     with ProgressBar(total=1) as progress:
-        fit(1, 1, img, 1, 0.1, progress)
+        fit(1, 1, img, 1, 0.1, 0.1, progress)
     enablePrint()
 
     with ProgressBar(total=maxiter) as progress:
-        kohonen_map = fit(length, breadth, img, maxiter, lr, progress)
+        kohonen_map = fit(length, breadth, img, maxiter, lr, spread, progress)
 
     return kohonen_map
 
 
+def retrieve_inputs():
+    global image_path, output_dir, root
+
+    try:
+
+        length = int(entry1.get())
+        breadth = int(entry2.get())
+        learning_rate = float(entry3.get())
+        maxiter = int(entry4.get())
+        neighbourhoodfunctionspread = float(entry5.get())
+    
+    except:
+        x = messagebox.showerror("Warning", "Incompatible Parameters Inputted, Check Readme")
+        exit(0)
+    
+    if(length < 0 or breadth < 0 or learning_rate < 0 or maxiter < 0 or neighbourhoodfunctionspread < 0):
+        x = messagebox.showerror("Warning", "Incompatible Parameters Inputted, Check Readme")
+        exit(0)
+
+    print(image_path)
+    print(output_dir)
+    
+    img_arr = None
+
+    if(image_path[-3:] == 'jpg' or image_path[-3:] == 'png'):
+        
+        plt.figure()
+        img = Image.open(image_path).convert('RGB')
+        img = img.resize((150, 150))
+        img.save(f'{output_dir}/150x150.jpg')
+        img_arr = np.asarray(img)
+
+    elif(image_path[-3:] == 'npy'):
+        
+        img_arr = np.load(image_path)
+
+    elif(image_path[-3:] == 'mat'):
+
+        mat = scipy.io.loadmat(image_path)
+        keys = list(mat.keys())
+        image = mat[keys[3]]
+        img_arr = np.array(image, dtype=np.int32)
+
+    else:
+
+        x = messagebox.showerror("Warning", "Wrong Input File Type")
+        exit(0)
+        
+
+    kohonen_map = generate_kohonen(img_arr, length, breadth, learning_rate, maxiter, neighbourhoodfunctionspread)
+    
+    if(kohonen_map.shape[2] == 3):
+        
+        plt.imshow(kohonen_map/255, interpolation='nearest')
+        plt.savefig(f'{output_dir}/kohonen.png')
+        
+    coded_image = generate_coded_image(img_arr, kohonen_map)
+    print(coded_image.shape)
+    np.save(f'{output_dir}/coded_image', coded_image)
+
+    if(kohonen_map.shape[2] == 3):
+        
+        plt.figure()
+        plt.axis('off')
+        img_restored = generate_image_from_coded(coded_image, kohonen_map)
+        plt.imshow(img_restored/255, interpolation='nearest')
+        plt.savefig(f'{output_dir}/restored.png')
+
+    print("Saved Files\n")
+    
+    print("Terminating Executable\n")
+    
+    exit(0)
+
+
 if __name__ == "__main__":
-    import numpy as np
-    from PIL import Image
-    import matplotlib.pyplot as plt
 
-    # for init image
-    plt.figure()
-    img = Image.open('initimg.png').convert('RGB')
-    img = img.resize((200, 200))
-    img.save('initimg_200x200.png')
-    img_arr = np.asarray(img)
-    kohonen_map = generate_kohonen(img_arr, 10, 10, 0.1, 10)
-    plt.imshow(kohonen_map/255, interpolation='nearest')
-    plt.savefig('initimg_kohonen.png')
-    coded_image = generate_coded_image(img_arr, kohonen_map)
-    np.save('initimg_coded_image', coded_image)
+    root = tk.Tk()
+    root.geometry("500x500")
+    root.withdraw()
 
-    plt.figure()
-    plt.axis('off')
-    img_restored = generate_image_from_coded(coded_image, kohonen_map)
-    plt.imshow(img_restored/255, interpolation='nearest')
-    plt.savefig('initimg_restored.png')
+    image_path = ''
+    output_dir = ''
 
-    # for mumbai satellite image
+    while len(image_path) == 0:
+        image_path = filedialog.askopenfilename(title="Select Image Filename")
 
-    plt.figure()
-    img = Image.open('mumbai.jpg').convert('RGB')
-    img = img.resize((200, 200))
-    img.save('mumbai_200x200.jpg')
-    img_arr = np.asarray(img)
-    kohonen_map = generate_kohonen(img_arr, 10, 20, 0.5, 100)
-    plt.imshow(kohonen_map/255, interpolation='nearest')
-    plt.savefig('mumbai_kohonen.png')
-    coded_image = generate_coded_image(img_arr, kohonen_map)
-    np.save('mumbai_coded_image', coded_image)
+    while len(output_dir) == 0:
+        output_dir = filedialog.askdirectory(title="Directory To Save Outputs")
 
-    plt.figure()
-    plt.axis('off')
-    img_restored = generate_image_from_coded(coded_image, kohonen_map)
-    plt.imshow(img_restored/255, interpolation='nearest')
-    plt.savefig('mumbai_restored.png')
+    root = tk.Tk()
+    root.geometry("500x500")
+    root.title("Input Parameters of Kohonen Map")
 
-    # generate coded image for hyperspectral image
-    import scipy.io
-    import numpy as np
-    mat = scipy.io.loadmat('KSC.mat')
-    image = mat['KSC']
-    image = np.array(image, dtype=np.int32)
-    coded_image = generate_kohonen(image, 10, 10, 0.4, 50)
-    np.save('KSC_coded_image', coded_image)
+    label1 = tk.Label(root, text="Length of Kohonen Map (Nodes)")
+    label1.pack()
+    entry1 = tk.Entry(root)
+    entry1.pack()
+
+    label2 = tk.Label(root, text="Breath of Kohonen Map (Nodes)")
+    label2.pack()
+    entry2 = tk.Entry(root)
+    entry2.pack()
+
+    label3 = tk.Label(root, text="Initial Learning Rate for Training")
+    label3.pack()
+    entry3 = tk.Entry(root)
+    entry3.pack()
+
+    label4 = tk.Label(root, text="Maximum Number of Iterations")
+    label4.pack()
+    entry4 = tk.Entry(root)
+    entry4.pack()
+
+    label5 = tk.Label(root, text="Neighbourhood Function Spread Factor")
+    label5.pack()
+    entry5 = tk.Entry(root)
+    entry5.pack()
+
+    # Create a button to retrieve inputs
+    button = tk.Button(root, text="Submit", command=retrieve_inputs)
+    button.pack()
+
+    # Start the Tkinter event loop
+    root.mainloop()
